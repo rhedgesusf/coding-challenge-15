2950ee99782c1d89406bf001b0ea418d7a364ad5
Riley Hedges <rhedges1@usf.edu>
2025-03-11 18:02:39 -0400

    Task 1 - Base Structure Setup

--- cc_15.js
+++ cc_15.js
@@ -1,2 +1,14 @@
+/////////////////////////////////////////
+// Task 1: Creating the Base Structure //
+/////////////////////////////////////////
 
+console.log("--------------------------------------");
+console.log("Task 1: Creating the Base Structure");
+
+// Created Base Structure in HTML
+
+ // get access to the dashboard
+ const riskDashboard = document.getElementById("riskDashboard");
+
+ console.log("Risk Dashboard Loaded")
 
